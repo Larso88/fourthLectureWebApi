6b5a785aee3680342d799b399e15048eca4667cb
Lars Ole Algrøy <Lars_ole_algroy@outlook.com>
2022-02-09 16:58:25 +0100

    program can now respond to request.

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+
+app.get("/login", (req, res) => {
+    res.json( {
+        username: "admin"
+    })
+})
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started at http://localhost:${server.address().port}`);
 })
