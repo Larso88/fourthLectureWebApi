1cea2d004dafac0f4127e7f2d46a6110672e344f
Lars Ole Algrøy <Lars_ole_algroy@outlook.com>
2022-02-09 17:09:54 +0100

    (sort of) respon to POST)

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get("/login", (req, res) => {
     })
 })
 
+app.post("/login", (req, res ) => {
+    // set something that GET/login returns user name
+    res.end()
+})
+
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started at http://localhost:${server.address().port}`);
 })
